adbd38d3dd04d8b0462a60ceb813e7caa0191c2c
Humane <33730758+HumaneIsTaken@users.noreply.github.com>
2019-05-28 14:44:47 +0200

    Phone-card-detail styling complete + plus added google font.

--- app/phone-catalogue/src/components/Phone.js
+++ app/phone-catalogue/src/components/Phone.js
@@ -5,42 +5,54 @@ const imageURL = 'http://localhost:3001/images/';
 
 function Phone(props) {
   
-  const [flipped, setFlipped] = useState("");
+  const [showDetail, setShowDetail] = useState("");
+  const phone = props.phone;
 
-  const flipCard = () => {
-    flipped === "flipped" ? setFlipped("") : setFlipped("flipped");
+  const toggleDetail = () => {
+    showDetail === "show-detail" ? setShowDetail("") : setShowDetail("show-detail");
   }
 
   const phoneCardFront = 
-    <div className="phone-card-front">
+    <div className="phone-card-overview">
+      <div className="focus-screen"></div>
       <img className="phone-image" 
-           src={`${imageURL}${props.phone.imageFileName}`}
+           src={`${imageURL}${phone.imageFileName}`}
            alt = {props.phone.name}
       /> 
       <div className="phone-info">
-        <p className="phone-maker">{props.phone.manufacturer}</p>
-        <p className="phone-name">{props.phone.name}</p>
-        <p className="phone-price">${props.phone.price}</p>    
+        <p className="phone-maker">{phone.manufacturer}</p>
+        <p className="phone-name">{phone.name}</p>
+        <p className="phone-price">${phone.price}</p>    
       </div>
     </div>
 
-  // TODO: Make this an actual card back.
   const phoneCardBack = 
-    <div className="phone-card-back">
+    <div className="phone-card-detail">
       <div className="phone-detail">
-        <p className="phone-detail-name">{props.phone.name}</p>
-        <p className="phone-detail-name">{props.phone.manufacturer}</p>
-        <p className="phone-detail-name">{props.phone.description}</p>
-        <p className="phone-detail-name">{props.phone.color}</p>
-        <p className="phone-detail-name">{props.phone.price}</p>
-        <p className="phone-detail-name">{props.phone.screen}</p>
-        <p className="phone-detail-name">{props.phone.processor}</p>
-        <p className="phone-detail-name">{props.phone.ram}</p>
+        <div className="phone-detail-header">
+          <div className="phone-detail-sth">
+            <p className="phone-detail-maker">{phone.manufacturer}</p>
+            <p className="phone-detail-name">{phone.name}</p>
+          </div>
+          <div className="phone-detail-spec">
+            <p className="phone-detail-screen">{phone.screen} screen</p>
+            <p className="phone-detail-cpu">{phone.processor} processor</p>
+            <p className="phone-detail-ram">{phone.ram}GB RAM</p>
+          </div>
+        </div>
+        <p className="phone-detail-desc">{phone.description}</p>
+        <div className="phone-detail-footer">
+          <p className="phone-detail-price">${phone.price}</p>
+          <div className="phone-detail-color">
+            <span>in</span>
+            <div className={`circle-color p-${phone.color}`}></div>
+          </div>
+        </div>
       </div>
     </div>
 
   return (
-    <div className={`phone-card ${flipped}`} onClick={flipCard}>
+    <div className={`phone-card ${showDetail}`} onClick={toggleDetail}>
       <div className="phone-card-inner">
         { phoneCardFront }
         { phoneCardBack }
